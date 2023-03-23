5acb3dc22a5213a06a4f71be07bbeb2c43f210b3
20jun01 <10junjun20@gmail.com>
2023-03-23 23:15:32 +0900

    feat: impl KeyInputReceiver

---
+++ scripts/Const.cs
@@ -0,0 +1,9 @@
+using System;
+
+public enum Direction {
+    None = -1,
+    Right,
+    Up,
+    Left,
+    Down
+}
---
+++ scripts/KeyInputReceiver.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class KeyInputReceiver : MonoBehaviour
+{
+    //singleton
+    private static KeyInputReceiver _instance;
+
+    public static KeyInputReceiver Instance {
+        get {
+            if (_instance == null) _instance = FindObjectOfType<KeyInputReceiver>();
+            return _instance;
+        }
+    }
+
+    private readonly KeyCode[] _directionKeyCodesWasd =
+    {
+        KeyCode.D, KeyCode.W, KeyCode.A, KeyCode.S
+    }; 
+    
+    private KeyCode _attackKeyCode = KeyCode.Space;
+
+    private readonly bool[] _directionKeyDownInput = new bool[4];
+    
+    private bool _attackKeyDownInput = false;
+
+    public bool[] DirectionKeyDownInput => _directionKeyDownInput;
+
+    public bool AttackKeyDownInput => _attackKeyDownInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _directionKeyDownInput[(int)Direction.Right] = 
+                                                       Input.GetKeyDown(_directionKeyCodesWasd[(int)Direction.Right]);
+        _directionKeyDownInput[(int)Direction.Up] =
+                                                    Input.GetKeyDown(_directionKeyCodesWasd[(int)Direction.Up]);
+        _directionKeyDownInput[(int)Direction.Left] = 
+                                                      Input.GetKeyDown(_directionKeyCodesWasd[(int)Direction.Left]);
+        _directionKeyDownInput[(int)Direction.Down] = 
+                                                      Input.GetKeyDown(_directionKeyCodesWasd[(int)Direction.Down]);
+        
+        _attackKeyDownInput = Input.GetKeyDown(_attackKeyCode);
+    }
+}
